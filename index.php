4e4b77329ea1cf3694735f1101ea3589dd994b0c
Andres Triviño <dylanalison123@gmail.com>
2023-03-09 17:41:35 -0500

    reall

---
+++ index.php
@@ -0,0 +1,284 @@
+<?php include 'header.php'; ?>
+<hr />
+<br />
+<br />
+<br />
+
+
+<div class="carousel fade-carousel slide" data-ride="carousel" data-interval="10000" id="bs-carousel">
+  <!-- Overlay 
+
+  <div class="overlay"></div>
+  -->
+
+  <!-- Indicators --> 
+  <ol class="carousel-indicators">
+    <li data-target="#bs-carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#bs-carousel" data-slide-to="1"></li>
+    <li data-target="#bs-carousel" data-slide-to="2"></li>
+  </ol>
+CS
+  <!-- Wrapper for slides -->
+
+  <div class="carousel-inner">
+    <div class="item slides active">
+      <div class="slide-1"></div>
+      <div class="hero">
+        <hgroup>
+          <h1 style="color:white">Chinauta Reall</h1>
+        </hgroup>
+      </div>
+      <div class="heroB">
+      <a href="habitaciones&tarifas.php" class="ov-btn-slide-left">Ver Habitaciones</a>
+      </div>
+      
+    </div>
+    <div class="item slides">
+      <div class="slide-2"></div>
+      <div class="hero">
+        <hgroup>
+          <h1 style="color:white">Planes Pasadía</h1>
+        </hgroup>
+      </div>
+      <div class="heroB">
+      <a href="planesPasadia.php" class="ov-btn-slide-left">Ver Planes Pasadía</a>
+      </div>
+    </div>
+    <div class="item slides">
+      <div class="slide-3"></div>
+      <div class="hero">
+        <hgroup>
+          <h1 style="color:white;">Bienvenidos</h1>
+          <h3>Conozca todass nuestras experienciass</h3>
+        </hgroup>
+      </div>
+      <div class="heroB2">
+      <a href="habitaciones&tarifas.php" class="ov-btn-slide-left">Ver Habitaciones</a>
+      <a href="planesPasadia.php" class="ov-btn-slide-left">Ver Planes Pasadía</a>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div class="spacer">
+  <div class="container" style="background-color: white">
+<h2 align="center">Conoce Nuestro Hotel</h2>
+  <div class="row align-items-center justify-content-center">
+  <div class="col-xs-1 "></div>
+    <div class="col-xs-10 ">
+      <div class="embed-responsive embed-responsive-16by9"><iframe width="70%" height="70%" src="https://www.youtube.com/embed/8EQokg8GwcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+    </div>
+    <div class="col-xs-1 "></div>
+  </div>
+  </div>
+</div>
+
+
+
+
+<!-- services -->
+<div class="spacer services wowload fadeInUp">
+
+  <h2 align="center">Descanso y diversión
+  </h2>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <!-- RoomCarousel -->
+        <div id="RoomCarousel" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="item active"><img src="images/photos/8.jpg" class="img-responsive" alt="slide"></div>
+            <div class="item  height-full"><img src="images/photos/1.jpg" class="img-responsive" alt="slide"></div>
+            <div class="item  height-full"><img src="images/photos/4.jpg" class="img-responsive" alt="slide"></div>
+          </div>
+          <!-- Controls -->
+          <a class="left carousel-control" href="#RoomCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+          <a class="right carousel-control" href="#RoomCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+        </div>
+        <!-- RoomCarousel-->
+        <div class="caption">Habitaciones<a href="habitaciones&tarifas.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
+      </div>
+
+
+      <div class="col-sm-4">
+        <!-- RoomCarousel -->
+        <div id="TourCarousel" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="item active"><img src="images/photos/6.jpg" class="img-responsive" alt="slide"></div>
+            <div class="item  height-full"><img src="images/photos/3.jpg" class="img-responsive" alt="slide"></div>
+            <div class="item  height-full"><img src="images/photos/50.jpg" class="img-responsive" alt="slide"></div>
+          </div>
+          <!-- Controls -->
+          <a class="left carousel-control" href="#TourCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+          <a class="right carousel-control" href="#TourCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+        </div>
+        <!-- RoomCarousel-->
+        <div class="caption">Entretenimiento<a href="gallery.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
+      </div>
+
+
+      <div class="col-sm-4">
+        <!-- RoomCarousel -->
+        <div id="FoodCarousel" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="item active"><img src="images/photos/55.jpg" class="img-responsive" alt="slide"></div>
+            <div class="item  height-full"><img src="images/photos/2.jpg" class="img-responsive" alt="slide"></div>
+            <div class="item  height-full"><img src="images/photos/20.jpg" class="img-responsive" alt="slide"></div>
+          </div>
+          <!-- Controls -->
+          <a class="left carousel-control" href="#FoodCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+          <a class="right carousel-control" href="#FoodCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+        </div>
+        <!-- RoomCarousel-->
+        <div class="caption">Espacios <a href="gallery.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- services -->
+
+<!-- Servicios -->
+<div class="spacer">
+  <div class="container services-features" style="background-color: white">
+    <h2 align="center">Nuestros Servicios:</h2>
+    <div class="boxesContainer">
+
+      <div class="cardBox">
+        <div class="card">
+          <div class="front">
+            <p>Servicio de:</p>
+            <h3>Piscinas</h3>
+            <p>Ver horarios</p>
+            <strong>&#x21bb;</strong>
+          </div>
+          <div class="back">
+            <p>✓ Piscinas Principales 9:00am a 5:00pm</p>
+            <p>✓ Tobogán: 11:30am 2:00pm 4:00pm</p>
+            <p>✓ Piscina Huéspedes 9:00am a 7:00pm</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="cardBox">
+        <div class="card">
+          <div class="front">
+            <p>Servicio de:</p>
+            <h3>Sauna y Turco</h3>
+            <p>Ver horarios</p>
+            <strong>&#x21bb;</strong>
+          </div>
+          <div class="back">
+            <p>✓ 10:30am a 12:00m y 3:00pm a 4:30pm</p>
+            <p style=" font-size: 0.9em;">✓ Aplican para fines de semana y temporada alta. Entre semana sujetos a ocupación del hotel (Previa reserva en recepción)</p>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="cardBox">
+        <div class="card">
+          <div class="front">
+            <p>Servicio de:</p>
+            <h3>Jacuzzis Exteriores</h3>
+            <p>Ver Información</p>
+            <strong>&#x21bb;</strong>
+          </div>
+          <div class="back">
+            <p>✓ Con Valor adicional de $60.000 hora – Capacidad 6 personas.
+            Reserva Previa en recepción del hotel</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="cardBox">
+        <div class="card">
+          <div class="front">
+            <p>Servicio de:</p>
+            <h3>Paseo Ecológico</h3>
+            <p>Ver horarios</p>
+            <strong>&#x21bb;</strong>
+          </div>
+          <div class="back">
+            <p>Caminata de tres kilómetros en descenso. Incluye: Piscina, lagos de pesca, caña de
+pescar y canasta aérea (sujetos a disponibilidad).
+</p>
+
+          </div>
+        </div>
+      </div>   
+
+    </div>
+
+    <div class="boxesContainer">
+
+<div class="cardBox">
+  <div class="card">
+    <div class="front">
+      <p>Servicio de:</p>
+      <h3>Salón de Juegos</h3>
+      <p>Ver horarios</p>
+      <strong>&#x21bb;</strong>
+    </div>
+    <div class="back">
+      <p>✓ Fines de Semana: 10:00am a 10:00pm</p>
+      <p>✓ Entre semana; 10:00am a 8:00pm </p>
+    </div>
+  </div>
+</div>
+
+<div class="cardBox">
+  <div class="card">
+    <div class="front">
+      <p>Servicio de:</p>
+      <h3>Gimnasio</h3>
+      <p>Ver horarios</p>
+      <strong>&#x21bb;</strong>
+    </div>
+    <div class="back">
+    <p> </p>
+    <p> </p>
+      <p>✓ 6:00am a 12:00pm</p>     
+    </div>
+  </div>
+</div>
+
+<div class="cardBox">
+  <div class="card">
+    <div class="front">
+      <p>Servicio de:</p>
+      <h3>Parqueadero</h3>
+      <p>Ver Información</p>
+      <strong>&#x21bb;</strong>
+    </div>
+    <div class="back">
+    <p> </p>
+    <p> </p>
+      <p>✓ Todo el día</p>
+    </div>
+  </div>
+</div>
+
+<div class="cardBox">
+  <div class="card">
+    <div class="front">
+      <p>Somos</p>
+      <h3>Pet Frendly</h3>
+      <p>Ver información</p>
+      <strong>&#x21bb;</strong>
+    </div>
+    <div class="back">
+    <p> </p>
+    <p> </p>
+    <p>✓ Avísanos con anticipación a fin de preparar el lugar para tu mascota</p>
+    </div>
+  </div>
+</div>   
+
+</div>
+  </div>
+</div>
+<!-- Find Servicios -->
+
+<?php include 'footer.php'; ?>
